d82f8c569ce17aa4586f36b1fcddb40cd52a4969
cymoux <51972571+cymoux@users.noreply.github.com>
2019-07-04 16:33:50 +0200

    Update and rename MainTest.java to main.java

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 import maze.MazeReadingException;
 import ui.MazeApp;
 
-public final class MainTest {
+public final class main {
 	public static void main(String[] args) throws MazeReadingException {
 		new MazeApp(); // creates a new window of the maze
 	}
